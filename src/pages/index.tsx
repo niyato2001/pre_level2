9108c9f5bf491a9d0f395b21229bce8be4e83665
Ryosuke Yoshizaki <ryosuke.yoshizaki@kikagaku.co.jp>
2022-03-12 22:14:47 +0900

    step0 の内容をブラッシュアップ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,12 @@ import { useCount } from '@/hook/useCount';
 const Home: React.FC = () => {
   const { count, countObj, countList, countUp, objCountUp, listCountUp } = useCount();
   return (
-    <main className='mx-auto max-w-lg py-6'>
-      <div className='grid gap-y-10'>
-        <h1 className='text-lg font-bold text-primary-800'>Rendeing Optimization of React</h1>
-        <DisplayNumber count={count} />
-        <DisplayObject countObj={countObj} />
-        <DisplayArray countList={countList} />
-        <DisplayFunction countUp={countUp} objCountUp={objCountUp} listCountUp={listCountUp} />
-      </div>
+    <main className='mx-auto grid max-w-lg gap-y-10 py-6'>
+      <h1 className='text-lg font-bold text-primary-800'>Rendeing Optimization of React</h1>
+      <DisplayNumber count={count} />
+      <DisplayObject countObj={countObj} />
+      <DisplayArray countList={countList} />
+      <DisplayFunction countUp={countUp} objCountUp={objCountUp} listCountUp={listCountUp} />
     </main>
   );
 };
